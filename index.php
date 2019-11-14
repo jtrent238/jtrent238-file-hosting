f144b0935d961946c9d8e6fc332bdb9fba93d26e
unknown <jtrent238@outlook.com>
2019-11-14 10:52:43 -0500

    111420191052

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html
+<html>
+	<head>
+		<title>jtrent238 File Hosting</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="assets/css/main.css">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css">
+	</head>
+	<body>
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+		    Select image to upload:
+		    <input type="file" name="fileToUpload" id="fileToUpload">
+		    <input type="submit" value="Upload Image" name="submit">
+		</form>
+	</body>
+</html>
