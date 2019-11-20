fb6ef7f280f8e448899de13549865ee96e367f13
unknown <jtrent238@outlook.com>
2019-11-20 14:06:23 -0500

    11202019206

--- index.php
+++ index.php
@@ -13,18 +13,18 @@
 		<div class="w3-container ">
 			<div class="w3-container">
 				<div class="w3-container">
-					<div class="w3-container w3-display-middle">
+					<div class="w3-container w3-display-middle upload_box">
 						<div class="w3-card w3-indigo">
 							<div class="w3-containter w3-blue">
 								<h2 class="w3-xxlarge w3-center">Upload a file!</h2>
 							</div>
 							<div class="w3-container">
 								<form class="form_upload w3-form" name="form_upload" action="upload.php" method="post" enctype="multipart/form-data">
-								    <label>Select image to upload:</label>
-								    
-								    <input class="inputUpload w3-input w3-border" type="file" name="fileToUpload" id="fileToUpload">
-								    </br>
-								    <input class="inputSubmit w3-input w3-border w3-button w3-blue" type="submit" value="Upload Image" name="submit">
+									<label>Select image to upload:</label>
+
+									<input class="inputUpload w3-input w3-border" type="file" name="fileToUpload" id="fileToUpload">
+									</br>
+									<input class="inputSubmit w3-input w3-border w3-button w3-blue" type="submit" value="Upload Image" name="submit">
 								</form>
 							</div>
 						</div>
@@ -32,11 +32,14 @@
 				</div>
 			</div>
 		</div>
+		</br>
+		</br>
 		<footer>
 			<div class="w3-container">
-				<div class="w3-container w3-center w3-display-bottommiddle">
+				<div class="w3-container w3-display-bottommiddle">
 					<p class="w3-text">
-						Site made by <a href="https://github.com/jtrent238">jtrent238</a>
+						Site made by
+						<a href="https://github.com/jtrent238">jtrent238</a>
 					</p>
 				</div>
 			</div>
